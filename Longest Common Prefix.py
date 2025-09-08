18770df27398bee80b969de01c7b2091929b0150
richus29 <55362080+richus29@users.noreply.github.com>
2025-09-08 13:42:23 +0300

    Update Longest Common Prefix.py

--- Longest Common Prefix.py
+++ Longest Common Prefix.py
@@ -1,28 +1,12 @@
 strs = ["flower","flow","flight"]
 
-"""answer = "".join(map(str, set.intersection(*[set(i) for i in strs])))"""
-
-"""for i in range(len(min(strs, key = len))):
-    if strs[0][i] in strs:
-        """
-
-"""for word in strs:
-    for j in range(len(min(strs, key = len))):
-        if word in strs[i-1][j]:
-            answer += (strs[0][i-1])
-            break
-    answer += i[0]"""
-for i in range(len(strs[0])-1):
-    answer = strs[0][i]
-    for j in range(1, len(strs)):
-        if i == len(strs[j]) or strs[j][i] !=answer:
-            print(strs[0][:i])
-            break
-#print(strs[0])
-
-"""
-answer = strs[0]
-for i in range(1, len(min(strs, key = len))-1):
-    answer = ''.join(set(answer).intersection(strs[i]))
+answer = ""
+strs = sorted(strs)
+print(strs)
+first = strs[0]
+last = strs[-1]
+for i in range(min(len(first),len(last))):
+    if (first[i] != last[i]):
+        print(answer)
+    answer += first[i]
 print(answer)
-"""
